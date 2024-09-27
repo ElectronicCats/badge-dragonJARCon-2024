7849b279b6a31bd817815254cda56b410dcf2e9f
Kevin Jahaziel Leon Morales <jahaziel.lem@gmail.com>
2024-09-27 10:45:05 -0500

    feat: No infection

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -40,7 +40,7 @@ void app_main() {
   menus_module_begin();
   leds_off();
   preferences_put_bool("wifi_connected", false);
-  infection_begin();
+  // infection_begin();
   // badge_link_module_begin();
   leds_notification();
   sbattery_set_status();
